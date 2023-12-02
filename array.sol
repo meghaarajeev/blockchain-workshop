4de29d74f8956b38cc3efe8c8f91dfd167bb0318
Megha Rajeev <106422400+meghaarajeev@users.noreply.github.com>
2023-12-02 15:03:08 +0530

    Create array.sol

---
+++ array.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.0;
+
+contract array{
+    uint [] public arr= new uint256 [] (100); //declaration of array
+    uint public s=0;
+    uint256 public count=0;
+
+    function addElement(uint256 x) public{
+
+        arr[count]=x;
+        count++;
+    }
+
+    function sum() public returns(uint256){
+        for(uint256 i=0; i<=count;i++){
+            s=s+arr[i]
+        }
+        return s;
+    }  
+}
