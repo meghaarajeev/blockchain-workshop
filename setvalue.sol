358be2eca89b73e1dbca319bd2e2ff1ccca61034
Megha Rajeev <106422400+meghaarajeev@users.noreply.github.com>
2023-12-02 15:43:37 +0530

    Update setvalue.sol

--- setvalue.sol
+++ setvalue.sol
@@ -1,1 +1,18 @@
 
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.0;
+
+contract mycontract{
+    string public name;
+    uint256 public a;
+    uint256 public b;
+
+    constructor() public {
+        name="hai";
+    }
+
+    function setValue(uint256 x, uint256 y)public{
+        a=x;
+        b=y;
+    }
+}
